38e821faad74cd69de74c3fcd13cb0017efffbb7
Pilar Argudo <pargudo@disid.com>
2015-09-17 17:31:14 +0200

    Custom init slick js. Styles for slick.

--- src/main/webapp/scripts/scripts.js
+++ src/main/webapp/scripts/scripts.js
@@ -1,3 +1,53 @@
+<<<<<<< HEAD
+// Init slick for overmenu & mediaqueries conditional
+
+$(document).ready(function(){
+    var width = $(window).width();
+    if( width >= 959 ){
+        // code for desktop view
+    	
+    	//init Slick
+  	  	$('.overmenu').slick({
+		  infinite: false,
+		  speed: 300,
+		  variableWidth: true,		  
+		  });
+  	  	
+  	  	//overwrite styles Smartmenu + Slick
+    	if ( $( 'ul.navbar-nav' ).hasClass( "overmenu" ) ) {
+    	      $('.sm ul').addClass('smartmenu-slick');
+    	};
+    
+    } 
+});
+
+$(window).resize(function() {
+        var width = $(window).width();
+        
+        
+});
+
+$(window).resize();
+
+//$(window).resize(function() {
+//    var width = $(window).width();
+//    if( width >= 959 ){
+//        // code for desktop view
+//    	$('body').css({ 
+//			  //css properties
+//			  "border": "10px solid orange",
+//		  });
+//    } else if( width >= 768 && width <= 959 ) {
+//        // code for tablet view        	
+//    } else if( width >= 480 && width <= 767 ) {
+//        // code for mobile landscape
+//    } else if( width <= 479 ) {
+//        // code for mobile portrait
+//    }
+//});
+//
+//$(window).resize();
+=======
 // Init slick for overmenu
 
 $(document).ready(function(){
@@ -10,3 +60,4 @@ $(document).ready(function(){
 
 
 
+>>>>>>> branch 'master' of https://github.com/pilarargudo/gvnix-responsive-menu
